4ef6b476f2069fe5b3181606a2a13b6859318500
James McConnell <james.mcc90@gmail.com>
2021-01-30 15:34:30 +0000

    Added test for Oracle queries/reporting

---
+++ OracleTest.py
@@ -0,0 +1,10 @@
+import cx_Oracle
+import pandas as pd
+
+dsn = cx_Oracle.makedsn('localhost', '1521', 'xe')
+conn = cx_Oracle.connect('SYS', '64lislunnan', dsn, cx_Oracle.SYSDBA)
+c = conn.cursor()
+script = 'SELECT * FROM CARS'
+script_output = pd.read_sql(script, conn).to_string()
+
+print(script_output)
