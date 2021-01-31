3124ad10bb13ce8e7fad410ab26dba4921ce7fdc
James McConnell <james.mcc90@gmail.com>
2021-01-31 21:38:09 +0000

    Added new SQL query. Testing outputting data from MongoDB to Excel.

--- MongoDB.py
+++ MongoDB.py
@@ -1,7 +1,21 @@
 from pymongo import MongoClient
-from pprint import pprint
+import csv
+import codecs
 
-client = MongoClient("mongodb+srv://james:McConnell%401990@sandbox.1iqe7.mongodb.net/test?authSource=admin&replicaSet=atlas-u2rtn2-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true")
-db = client.james
-serverStatusResult = db.command("serverStatus")
-pprint(serverStatusResult)
+mongo_client = MongoClient("mongodb+srv://james:McConnell%401990@sandbox.1iqe7.mongodb.net/test?authSource=admin&replicaSet=atlas-u2rtn2-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true")
+
+db = mongo_client.sample_airbnb
+col = db.listingsAndReviews
+cursor = col.find()
+
+with codecs.open('C:\\Users\\James\\Desktop\\test.csv', 'w', 'utf-8') as csvfile:
+    columns = ["Name", "Summary"]
+    writer = csv.writer(csvfile)
+    writer.writerows("")
+    writer.writerow(columns)
+
+    for data in cursor:
+
+        writer.writerows([[data["name"],
+                           data["summary"]
+                           ]])
--- OracleTest.py
+++ OracleTest.py
@@ -4,7 +4,10 @@ import pandas as pd
 dsn = cx_Oracle.makedsn('localhost', '1521', 'xe')
 conn = cx_Oracle.connect('SYS', '64lislunnan', dsn, cx_Oracle.SYSDBA)
 c = conn.cursor()
-script = 'SELECT * FROM CARS'
+
+script = "SELECT * FROM CARS " \
+         "WHERE CAR_MAKE LIKE 'V%' " \
+         "AND CAR_COLOUR LIKE '%Bl%'"
 script_output = pd.read_sql(script, conn).to_string()
 
 print(script_output)
